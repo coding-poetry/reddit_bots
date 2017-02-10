8b74249c3d369077feddb3b29d561ef7a0766849
harrelchris <harrelchris@gmail.com>
2017-02-10 03:36:23 -0600

    Update poster.py

--- bots/mirror_bot/poster.py
+++ bots/mirror_bot/poster.py
@@ -1,58 +1,161 @@
-from time import sleep, strftime, time
-import configparser
+from time import sleep
 import prawcore
 import logging
+import sqlite3
+import config
 import praw
 
-start = time()
-config = configparser.ConfigParser()
-config.read('bot.conf', encoding='utf-8')
-AUTH = config['AUTHENTICATION']
-LOG = config['LOGGING']
-logging.basicConfig(filename=LOG['logfile'], level=logging.DEBUG, format='%(asctime)s %(message)s')
-logger = logging.getLogger(__name__)
-reddit = praw.Reddit(
-    password=AUTH['password'],
-    username=AUTH['username'],
-    client_id=AUTH['client_id'],
-    user_agent=AUTH['user_agent'],
-    client_secret=AUTH['client_secret'])
+logger = logging.getLogger('Poster')
+reddit = praw.Reddit(**config.poster)
 
-logger.disabled = False
-max_restarts = 5
-wait = 60
+restarts = 0
+loiter = config.loiter
+max_restarts = config.max_restarts
+
+logging.basicConfig(filename=config.log_file,
+                    filemode=config.mode,
+                    level=config.level,
+                    format='%(asctime)s %(levelname)s %(name)s %(message)s')
+logger.disabled = config.disabled
+conn = sqlite3.connect(config.db_file)
+c = conn.cursor()
 
 
-def run():
-    # Stream comments or submissions
-    pass
+# --- Post submissions
+def new_subs():
+    """Get all new submissions and post them"""
+    c.execute('''SELECT * FROM submissions WHERE posted=0''')
+    for submission in c.fetchall():
+        yield submission
+
+
+def post_sub(submission):
+    """Cross post the submission as a selftext or link and return the repost_id"""
+    title = submission[5]
+    text = submission[6]
+    link = submission[7]
+    destination = reddit.subreddit(config.dest)
+    if text:
+        post = destination.submit(title=title, selftext=text, send_replies=False)
+    else:
+        post = destination.submit(title=title, url=link, send_replies=False)
+    return post.id
+
+
+def mark_sub_posted(sub, repost_id):
+    """Update posted and repost_id"""
+    c.execute('''UPDATE submissions SET posted=(?), repost_id=(?) WHERE id=(?)''',
+              (True, repost_id, sub[1]))
+    conn.commit()
+
+
+# --- Handle root comments
+def new_root_comments():
+    """Get all new root comments and yield them"""
+    c.execute('''SELECT * FROM comments WHERE is_root=1 AND posted=0''')
+    for comment in c.fetchall():
+        yield comment
+
+
+def parent_submission(comment):
+    sub_id = comment[7]
+    c.execute('''SELECT repost_id FROM submissions WHERE id=(?)''', (sub_id,))
+    entry = c.fetchone()
+    if entry and entry[0]:
+        return entry[0]
+    return False
+
+
+def post_root_comment(parent, root_comment):
+    repost = reddit.submission(parent).reply(root_comment[5])
+    return repost.id
+
+
+def mark_root_posted(root_comment, repost_id):
+    """Update as posted and insert repost_id"""
+    c.execute('''UPDATE comments SET posted=(?), repost_id=(?) WHERE id=(?)''',
+              (True, repost_id, root_comment[1]))
+    conn.commit()
+
+
+# --- Handle child comments
+def new_child_comments():
+    """Get all new child comments and yield them"""
+    c.execute('''SELECT * FROM comments WHERE is_root=0 AND posted=0''')
+    for comment in c.fetchall():
+        yield comment
+
+
+def parent_comment(child_comment):
+    """Return the parent comment origi"""
+    parent_comm = child_comment[6]
+    c.execute('''SELECT repost_id FROM comments WHERE id=(?)''', (parent_comm,))
+    entry = c.fetchone()
+    if entry and entry[0]:
+        return entry[0]
+    return False
+
+
+def post_child_comment(parent, child_comment):
+    repost = reddit.comment(parent).reply(child_comment[5])
+    return repost.id
+
+
+def mark_child_posted(child_comment, repost_id):
+    """Update as posted and insert repost_id"""
+    c.execute('''UPDATE comments SET posted=(?), repost_id=(?) WHERE id=(?)''',
+              (True, repost_id, child_comment[1]))
+    conn.commit()
+
+
+# --- Main
+def main():
+    """Cross post all new submissions, then root comments, then child comments"""
+    for sub in new_subs():
+        repost_id = post_sub(sub)
+        mark_sub_posted(sub, repost_id)
+
+    for root_comment in new_root_comments():
+        parent = parent_submission(root_comment)
+        if parent:
+            repost_id = post_root_comment(parent, root_comment)
+            mark_root_posted(root_comment, repost_id)
+
+    for child_comment in new_child_comments():
+        parent = parent_comment(child_comment)
+        if parent:
+            repost_id = post_child_comment(parent, child_comment)
+            mark_child_posted(child_comment, repost_id)
 
 
 if __name__ == '__main__':
     logger.info('Start')
-    restarts = 0
     while restarts < max_restarts:
         try:
-            run()
+            main()
+            sleep(loiter)
         except prawcore.exceptions.OAuthException as e:
             logger.exception(e)
             break
         except prawcore.exceptions.RequestException as e:
             logger.exception(e)
             restarts += 1
-            sleep(wait)
+            sleep(loiter)
             continue
         except prawcore.exceptions.ResponseException as e:
             logger.exception(e)
             restarts += 1
-            sleep(wait)
+            sleep(loiter)
             continue
         except Exception as e:
             logger.exception(e)
             restarts += 1
-            sleep(wait)
+            sleep(loiter)
             continue
-        else:
-            logger.info('Max restarts exceeded')
-
+    else:
+        logger.info('Max restarts exceeded')
     logger.info('Stop')
+    if config.admin_user:
+        reddit.redditor(config.admin_user).message('YOUR BOT HAS STOPPED',
+                                                   'Your Poster bot has malfunctioned.\n'
+                                                   'Please review the activity log for errors.')
