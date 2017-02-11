4da8603a87118afc42fe6b11be8dba2060aeb289
harrelchris <harrelchris@gmail.com>
2017-02-11 07:18:48 -0600

    Refactor Config

--- bots/mirror_bot/config.py
+++ bots/mirror_bot/config.py
@@ -7,6 +7,8 @@ logger = the user that will watch, but never post anything
 poster = the user that will create crossposts but cannot see the private sub
 """
 
+# Leave blank or enter a reddit username to receive error notifications
+# Must be different from logger and poster accounts
 admin_user = ''
 
 logger = {
@@ -25,17 +27,16 @@ poster = {
     'user_agent': 'string'
 }
 
+src = ''
+dest = ''
 
 # --- Do not modify these values --- #
 loiter = 60
 max_restarts = 5
-
-src = ''
-dest = ''
 
 log_file = 'activity_log.txt'
 level = 20
 disabled = False
 mode = 'a'
 
-db_file = 'creation_bot.db'
+db_file = 'mirror_bot.db'
