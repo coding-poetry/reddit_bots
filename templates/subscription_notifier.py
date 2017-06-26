9f77f2a0a0e5acda788cb16a26c8203e8386e2e5
KimPeek <kimpeek@users.noreply.github.com>
2017-06-26 07:51:07 -0500

    Create subscription_notifier.py

---
+++ templates/subscription_notifier.py
@@ -0,0 +1,28 @@
+"""
+if new post in subreddit:
+    message user with permalink
+"""
+
+from time import time
+import praw
+
+user = 'username'
+sub = 'subreddit'
+start = time()
+reddit = praw.Reddit('AUTHENTICATION')
+
+
+def handle(submission):
+    link = 'https://www.reddit.com{}'.format(submission.permalink)
+    body = "[{}]({})".format(submission.title, link)
+    reddit.redditor(user).message("New submission", body)
+
+
+def main():
+    submission_stream = reddit.subreddit(sub).stream.submissions()
+    for submission in submission_stream:
+        if submission.created_utc > start:
+            handle(submission)
+
+if __name__ == '__main__':
+    main()
