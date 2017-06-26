c82818ab687810b1935baa124a79194edd4ea155
KimPeek <kimpeek@users.noreply.github.com>
2017-06-26 05:18:54 -0500

    Create submission_comment_feed.py

---
+++ templates/RedditStream/submission_comment_feed.py
@@ -0,0 +1,26 @@
+import praw
+from RedditStream import Stream
+
+reddit = praw.Reddit('AUTHENTICATION')
+sub = 'all'
+stream = Stream(reddit, sub)
+
+
+def handle_submission(submission):
+    print(submission.author.name)
+
+
+def handle_comment(comment):
+    print(comment.author.name)
+
+
+def main():
+    for item in stream:
+        submission, comment = item
+        if submission:
+            handle_submission(submission)
+        else:
+            handle_comment(comment)
+
+if __name__ == '__main__':
+    main()
