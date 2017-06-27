83fb3e3cff453ced3de51f8e650cc02baa36039c
KimPeek <kimpeek@users.noreply.github.com>
2017-06-27 03:13:05 -0500

    Update live.py

--- commands/live.py
+++ commands/live.py
@@ -1,16 +1,24 @@
 import praw
 
 reddit = praw.Reddit('AUTHENTICATION')
+LIVE_ID = 'abc123'
 
 reddit.live.create(title, description=None, nsfw=False, resources=None)
 reddit.live.info(ids)
 reddit.live.now()
 
-live = reddit.live('ukaeu1ik4sw5')
+live = reddit.live(LIVE_ID)
 live.contrib()
+live.contrib.add(body)
+live.contrib.close()
+live.contrib.thread()
+live.contrib.update(title=None, description=None, nsfw=None, resources=None, **other_settings)
 live.contributor()
-live.discussions()
-live.fullname()
-live.id()
-live.report()
-live.updates()
+live.contributor.accept_invite()
+live.contributor.invite(redditor, permissions=None)
+live.contributor.leave()
+live.contributor.remove(redditor)
+live.contributor.remove_invite(redditor)
+live.contributor.thread()
+live.contributor.update(redditor, permissions=None)
+live.contributor.update_invite(redditor, permissions=None)
