381f498a3829de52208e1b7a4a6eaab1f2ce05f1
KimPeek <kimpeek@users.noreply.github.com>
2017-06-28 10:27:04 -0500

    Update redditor.py

--- commands/redditor.py
+++ commands/redditor.py
@@ -4,32 +4,64 @@ reddit = praw.Reddit('AUTHENTICATION')
 USERNAME = 'abc123'
 
 redditor = reddit.redditor(USERNAME)
+redditor.comment_karma
 redditor.comments()
 redditor.comments.controversial(time_filter='all')  # 'all', 'day', 'hour', 'month', 'week', 'year'
 redditor.comments.hot()
 redditor.comments.new()
 redditor.comments.top(time_filter='all')  # 'all', 'day', 'hour', 'month', 'week', 'year'
-redditor.controversial(time_filter='all')  # 'all', 'day', 'hour', 'month', 'week', 'year'
-redditor.downvoted()
-redditor.friend(note=None)
-redditor.friend_info()
-redditor.fullname()
-redditor.gild(months=1)
-redditor.gilded()
-redditor.gildings()
-redditor.hidden()
-redditor.hot()
-redditor.message(subject, message, from_subreddit=None)
-redditor.multireddits()
-redditor.name()
-redditor.new()
-redditor.saved()
+redditor.controversial
+redditor.created
+redditor.created_utc
+redditor.downvoted
+redditor.features
+redditor.friend
+redditor.friend_info
+redditor.from_data
+redditor.fullname
+redditor.gild
+redditor.gilded
+redditor.gildings
+redditor.gold_creddits
+redditor.gold_expiration
+redditor.has_mail
+redditor.has_mod_mail
+redditor.has_subscribed
+redditor.has_verified_email
+redditor.hidden
+redditor.hide_from_robots
+redditor.hot
+redditor.id
+redditor.in_beta
+redditor.inbox_count
+redditor.is_employee
+redditor.is_friend
+redditor.is_gold
+redditor.is_mod
+redditor.is_sponsor
+redditor.is_suspended
+redditor.link_karma
+redditor.message
+redditor.modhash
+redditor.multireddits
+redditor.name
+redditor.new
+redditor.new_modmail_exists
+redditor.over_18
+redditor.parse
+redditor.pref_no_profanity
+redditor.pref_show_snoovatar
+redditor.pref_top_karma_subreddits
+redditor.saved
 redditor.submissions()
 redditor.submissions.controversial(time_filter='all')  # 'all', 'day', 'hour', 'month', 'week', 'year'
 redditor.submissions.hot()
 redditor.submissions.new()
 redditor.submissions.top(time_filter='all')  # 'all', 'day', 'hour', 'month', 'week', 'year'
-redditor.top(time_filter='all')  # 'all', 'day', 'hour', 'month', 'week', 'year'
-redditor.unblock()
-redditor.unfriend()
-redditor.upvoted()
+redditor.subreddit
+redditor.suspension_expiration_utc
+redditor.top
+redditor.unblock
+redditor.unfriend
+redditor.upvoted
+redditor.verified
