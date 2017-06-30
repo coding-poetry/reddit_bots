84ec6f6a75bb45f08a2e27cd51232fb634fbf67e
KimPeek <kimpeek@users.noreply.github.com>
2017-06-30 13:35:23 -0500

    Update submission_feed.py

--- templates/submission_feed.py
+++ templates/submission_feed.py
@@ -1,20 +1,43 @@
 from time import time
 import praw
 
-sub = 'all'
-start = time()
 reddit = praw.Reddit('AUTHENTICATION')
-
-
-def handle(submission):
-    print(submission.author.name)
+subreddit = 'all'
 
 
 def main():
-    submission_stream = reddit.subreddit(sub).stream.submissions()
-    for submission in submission_stream:
+    start = time()
+    for submission in reddit.subreddit(subreddit).stream.submissions():
         if submission.created_utc > start:
             handle(submission)
+
+
+def handle(submission):
+    title = submission.title
+    url = submission.url if not submission.is_self else None
+    selftext = submission.selftext if submission.is_self else None
+    created_utc = submission.created_utc
+    permalink = submission.permalink
+    shortlink = submission.shortlink
+    _id = submission.id
+
+    author = submission.author
+    verified = submission.author.verified
+    author_age = submission.author.created_utc
+    link_karma = submission.author.link_karma
+    comment_karma = submission.author.comment_karma
+
+    submission.author.friend(note=None)
+    submission.reply(body)
+    submission.report(reason)
+    submission.author.message(subject, message, from_subreddit=None)
+
+    submission.mod.approve()
+    submission.mod.distinguish(how='yes', sticky=False)  # yes, no, admin, special
+    submission.mod.ignore_reports()
+    submission.mod.remove(spam=False)
+    submission.mod.sticky(state=True, bottom=True)
+
 
 if __name__ == '__main__':
     main()
