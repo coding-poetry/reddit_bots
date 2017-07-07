0c296ae41e2d2cb7cc6bcbfbb7ae098f7ab759dd
KimPeek <kimpeek@users.noreply.github.com>
2017-07-07 13:00:54 -0500

    Update unhide_hidden.py

--- templates/unhide_hidden.py
+++ templates/unhide_hidden.py
@@ -2,6 +2,14 @@ import praw
 
 reddit = praw.Reddit('AUTHENTICATION')
 
-for post in reddit.user.me().hidden(limit=None):
-    post.unhide()
-    
+while True:
+    objects = [post for post in reddit.user.me().hidden(limit=None)]
+    try:
+        first = objects[0]
+    except IndexError:
+        break
+    else:
+        print('{} hidden objects remaining'.format(len(objects)))
+        first.unhide(other_submissions=objects[1:40])
+
+print('No hidden objects remaining')
