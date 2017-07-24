a64a29166a0e9de18bca884aaf79d90610c75e36
KimPeek <kimpeek@users.noreply.github.com>
2017-07-23 21:45:55 -0500

    Update RedditStream.py

--- templates/RedditStream/RedditStream.py
+++ templates/RedditStream/RedditStream.py
@@ -1,52 +1,57 @@
+import time
 import threading
 from collections import deque
+from prawcore.exceptions import PrawcoreException
 
 
 class Stream:
     """A generator class of submissions and comments"""
 
     def __init__(self, reddit, subreddit):
-        self._reddit = reddit
-        self._subreddit = subreddit
-        self._queue = deque()
-        self._subreddit_stream()
+        self.reddit = reddit
+        self.subreddit = subreddit
+        self.queue = deque()
+        self.startup()
 
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        while True:
-            try:
-                return self._queue.pop()
-            except IndexError:
-                continue
-
-    def _queue_submissions(self):
+    def queue_submissions(self):
         """Stream all submissions and put them into the queue"""
 
-        _subreddit = self._reddit.subreddit(self._subreddit)
+        sub = self.reddit.subreddit(self.subreddit)
         while True:
+            start = time.time()  # Avoid old data
             try:
-                for submission in _subreddit.stream.submissions():
-                    self._queue.append((submission, False))
-            except Exception as e:
+                for submission in sub.stream.submissions():
+                    if submission.created_utc <= start:
+                        continue
+                    self.queue.append((submission, False))
+            except PrawcoreException:
                 pass
 
-    def _queue_comments(self):
+    def queue_comments(self):
         """Stream all comments and put them into the queue"""
 
-        _subreddit = self._reddit.subreddit(self._subreddit)
+        sub = self.reddit.subreddit(self.subreddit)
         while True:
+            start = time.time()  # Avoid old data
             try:
-                for comment in _subreddit.stream.comments():
-                    self._queue.append((False, comment))
-            except Exception as e:
+                for comment in sub.stream.comments():
+                    if comment.created_utc <= start:
+                        continue
+                    self.queue.append((False, comment))
+            except PrawcoreException:
                 pass
 
-    def _subreddit_stream(self):
-        """Create a queue to use as a funnel, start the threads and yield the objects"""
+    def startup(self):
+        """Create a queue to use as a funnel, start the threads and puts the objects into the queue"""
 
-        submission_thread = threading.Thread(target=self._queue_submissions)
-        comment_thread = threading.Thread(target=self._queue_comments)
+        submission_thread = threading.Thread(target=self.queue_submissions)
+        comment_thread = threading.Thread(target=self.queue_comments)
         submission_thread.start()
         comment_thread.start()
+
+    def dual_stream(self):
+        while True:
+            try:
+                yield self.queue.pop()
+            except IndexError:
+                continue
